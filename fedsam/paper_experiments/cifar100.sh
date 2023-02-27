58f89cdc25758b0c3af5278b723a3e1e27c693d8
FlavioPatti <s301104@studenti.polito.it>
2023-02-27 13:12:51 +0100

    fixed param

--- fedsam/paper_experiments/cifar100.sh
+++ fedsam/paper_experiments/cifar100.sh
@@ -2,12 +2,12 @@
 
 pushd ../models
 
-declare -a alphas=("0" "0.05")
+declare -a alphas=("0" "0.5" "1000")
 
 function run_fedavg() {
   echo "############################################## Running FedAvg ##############################################"
   alpha="$1"
-  python main.py -dataset cifar100 --num-rounds 500 --eval-every 100 --batch-size 128 --num-epochs 3 --clients-per-round 5 -model resnet20 -lr 0.1 --weight-decay 0.0001 -device cuda:0 -algorithm fedopt --server-lr 1 --server-opt sgd --num-workers 0 --where-loading init -alpha ${alpha}
+  python main.py -dataset cifar100 --num-rounds 1000 --eval-every 100 --batch-size 128 --num-epochs 1 --clients-per-round 10 -model resnet20 -lr 0.1 --weight-decay 0.0001 -device cuda:0 -algorithm fedopt --server-lr 1 --server-opt sgd --num-workers 0 --where-loading init -alpha ${alpha}
 }
 
 #function run_fedavg_with_swa() {
